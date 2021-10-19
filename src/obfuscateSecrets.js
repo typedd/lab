1a3e0f784d934aa2fb4e797efa098828ecb416fc
unknown <mariaspiiish@gmail.com>
2021-10-19 17:11:33 +0400

    added some code but not a solution

--- src/obfuscateSecrets.js
+++ src/obfuscateSecrets.js
@@ -14,22 +14,14 @@ type Y = {
 }
 */
 
-const a /* : X */ = {}; // лезет
-const b /* : X */ = { x: 100 }; // не лезет
-const c /* : X */ = { secret: "asdf" }; // лезет
-const c1 /* : X */ = {secret: "asdf", a: 'b'}; // лезет
-const c2 /* : X */ = {secret: "asdf", a: 2}; // лезет
-const d /* : X */ = 'Hello'; // не лезет
-const e /* : X */ = {['Hello']: {}} // лезет
-const f /* : Y */ = {secret: '*****'} // лезет
-const g /* : Y */ = {['Hello']: {}} // лезет
-const h /* : Y */ = {secret: 'asdf'} // не лезет
-
-
-
 function obfuscateSecrets(obj /* : X */) /* : Y */ {
-    return obj;
-}
+    for (let key in obj) {
+            if (key === 'secret') {
+                obj[key] = '*****';
+            }
+        }
+        return obj;
+    };
 
 module.exports = {
     obfuscateSecrets
