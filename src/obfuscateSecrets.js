4fac830f3d154456f302ef31f8407823224020b1
unknown <mariaspiiish@gmail.com>
2021-10-20 13:08:33 +0400

    cheching the most simple solution

--- src/obfuscateSecrets.js
+++ src/obfuscateSecrets.js
@@ -15,22 +15,13 @@ type Y = {
 */
 
 function obfuscateSecrets(obj /* : X */) /* : Y */ {
-<<<<<<< HEAD
-   return {
+    return {
         secret: '*****',
         x: { 
             secret: '*****'
         }
-}
-=======
-    for (let key in obj) {
-        if (key === 'secret') {
-            obj[key] = '*****';
-        }
-    }
-    return obj;
+   }
 };
->>>>>>> e8a4b11b2ad4f287d4958e2792406dc0bdf3b3fd
 
 module.exports = {
     obfuscateSecrets
